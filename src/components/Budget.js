8b180338203ca714c71b654d733a5ea00e7a6211
sjiang20 <sjiang086@gmail.com>
2023-08-30 23:02:11 -0700

    currency

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,39 @@
 
+import React, { useContext,useState } from 'react';
+import { AppContext } from '../context/AppContext';
+const Budget = () => {
+    const { budget} = useContext(AppContext);
+  
+
+    const [cost, setCost] = useState('');
+
+    const handleCostChange = (event) => {
+        setCost(event.target.value)
+
+        if(budget < cost) {
+            alert("You cannot reduce budget value lower than the spending!");
+        
+            return;
+        }
+           };
+    
+
+   
+
+    return (
+        <div className='alert alert-secondary'>
+            <span>Budget: £</span>
+            <input
+                required='required'
+                type='number'
+                id='cost'
+                value= {cost}
+                style={{ marginLeft: '0.5rem' , size: 10}}
+                onChange={handleCostChange}
+                >
+                
+            </input>
+        </div>
+    );
+};
+export default Budget;
